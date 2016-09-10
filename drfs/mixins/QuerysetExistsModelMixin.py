d348342ebab4a66aa67a2807af5d238905ac06d1
Gerasev Kirill <gerasev.kirill@gmail.com>
2016-09-10 21:11:00 +0300

    [*] путь для QuerysetExistsModelMixin

--- drfs/mixins/QuerysetExistsModelMixin.py
+++ drfs/mixins/QuerysetExistsModelMixin.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 
 class QuerysetExistsModelMixin(object):
 
-    @list_route(methods=['get'])
+    @list_route(methods=['get'], url_path='querysetExists')
     def queryset_exists(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         return Response({
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-framework-generator',
-    version='0.4.15',
+    version='0.4.16',
     packages=['drfs'],
     include_package_data=True,
     license='BSD License',
