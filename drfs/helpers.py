556bfa13a84ba411e20c3e69c7aaf8d5081362d5
Gerasev Kirill <gerasev.kirill@gmail.com>
2018-11-10 08:11:25 +0200

    [^*]

--- drfs/helpers.py
+++ drfs/helpers.py
@@ -4,6 +4,8 @@
 
 def import_class(cl):
     cl = str(cl)
+    if '.site-packages.' in cl:
+        cl = cl.split('.site-packages.')[-1]
     # http://stackoverflow.com/questions/547829/how-to-dynamically-load-a-python-class
     d = cl.rfind(".")
     classname = cl[d+1:len(cl)]
