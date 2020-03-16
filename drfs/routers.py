d3e711b9b639b1a75d5eb4d9182baf42c534c136
Gerasev Kirill <gerasev.kirill@gmail.com>
2020-03-16 19:21:36 +0200

    [+] routers: trailing slash

--- drfs/routers.py
+++ drfs/routers.py
@@ -1,9 +1,12 @@
 
+from collections import namedtuple
 from django.core.exceptions import ImproperlyConfigured
 from rest_framework.routers import SimpleRouter as BaseSimpleRouter, Route, DynamicDetailRoute, DynamicListRoute
 from rest_framework.routers import flatten, replace_methodname
 
+from django.conf.urls import url
 
+RouteDrfs = namedtuple('Route', ['url', 'mapping', 'name', 'initkwargs', 'trailing_slash'])
 
 class SimpleRouter(BaseSimpleRouter):
     def get_routes(self, viewset):
@@ -42,6 +45,7 @@ class SimpleRouter(BaseSimpleRouter):
                 initkwargs.update(method_kwargs)
                 url_path = initkwargs.pop("url_path", None) or methodname
                 url_name = initkwargs.pop("url_name", None) or url_path
+                trailing_slash = initkwargs.pop("trailing_slash", None)
                 full_url = replace_methodname(route.url, url_path)
                 if ret.get(full_url, None):
                     for httpmethod in httpmethods:
@@ -54,11 +58,12 @@ class SimpleRouter(BaseSimpleRouter):
                 else:
                     ret[full_url] = {
                         'url': full_url,
+                        'trailing_slash': trailing_slash,
                         'mapping': {httpmethod: methodname for httpmethod in httpmethods},
                         'name': replace_methodname(route.name, url_name),
                         'initkwargs': initkwargs,
                     }
-            return [Route(**params) for fulr, params in ret.items()]
+            return [RouteDrfs(**params) for fulr, params in ret.items()]
 
 
             ret = []
@@ -68,15 +73,12 @@ class SimpleRouter(BaseSimpleRouter):
                 initkwargs.update(method_kwargs)
                 url_path = initkwargs.pop("url_path", None) or methodname
                 url_name = initkwargs.pop("url_name", None) or url_path
-                ret.append(Route(
+                ret.append(RouteDrfs(
                     url=replace_methodname(route.url, url_path),
                     mapping={httpmethod: methodname for httpmethod in httpmethods},
                     name=replace_methodname(route.name, url_name),
                     initkwargs=initkwargs,
                 ))
-            if 'CmsBlogEntry' in viewset.__name__:
-                print(ret)
-                print()
             return ret
 
         ret = []
@@ -90,5 +92,49 @@ class SimpleRouter(BaseSimpleRouter):
             else:
                 # Standard route
                 ret.append(route)
+
+        return ret
+
+
+    def get_urls(self):
+        """
+        Use the registered viewsets to generate a list of URL patterns.
+        """
+        ret = []
+
+        for prefix, viewset, basename in self.registry:
+            lookup = self.get_lookup_regex(viewset)
+            routes = self.get_routes(viewset)
+
+            for route in routes:
+
+                # Only actions which actually exist on the viewset will be bound
+                mapping = self.get_method_map(viewset, route.mapping)
+                if not mapping:
+                    continue
+
+                # Build the url pattern
+                if isinstance(route, RouteDrfs):
+                    trailing_slash = route.trailing_slash
+                    if trailing_slash is None:
+                        trailing_slash = self.trailing_slash
+                else:
+                    trailing_slash = self.trailing_slash
+                regex = route.url.format(
+                    prefix=prefix,
+                    lookup=lookup,
+                    trailing_slash=trailing_slash
+                )
+
+                # If there is no prefix, the first part of the url is probably
+                #   controlled by project's urls.py and the router is in an app,
+                #   so a slash in the beginning will (A) cause Django to give
+                #   warnings and (B) generate URLS that will require using '//'.
+                if not prefix and regex[:2] == '^/':
+                    regex = '^' + regex[2:]
+
+                view = viewset.as_view(mapping, **route.initkwargs)
+                name = route.name.format(basename=basename)
+                ret.append(url(regex, view, name=name))
 
         return ret
